b8881ee002824a06bc75564501eae9be7112650d
FatemaKotb <Fatema.Kotb02@eng-st.cu.edu.eg>
2024-04-20 12:44:07 +0200

    Initialized the backend project using Spring Initializer.

---
+++ src/main/java/com/APT/online/collaborative/text/editor/OnlineCollaborativeTextEditorApplication.java
@@ -0,0 +1,13 @@
+package com.APT.online.collaborative.text.editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCollaborativeTextEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCollaborativeTextEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/APT/online/collaborative/text/editor/OnlineCollaborativeTextEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APT.online.collaborative.text.editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCollaborativeTextEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
