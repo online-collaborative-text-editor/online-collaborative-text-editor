aaa3821ab2d191cb2654e5c70bd241197de897ad
HanzadaFayez <hanzada.eldesouky02@eng-st.cu.edu.eg>
2024-05-10 01:00:50 +0300

    changed owner to owned

--- src/main/java/com/APT/online/collaborative/text/editor/Controller/DocumentController.java
+++ src/main/java/com/APT/online/collaborative/text/editor/Controller/DocumentController.java
@@ -42,13 +42,13 @@ public class DocumentController {
         return ResponseEntity.ok("Document with ID " + documentId + " was deleted successfully.");
     }
 
-   @GetMapping("/list/viewer")
+   @GetMapping("/list/viewed")
     public ResponseEntity<List<DocumentDTO>> listViewerDocuments(@RequestAttribute("username") String username) {
         List<DocumentDTO> documents = documentService.listViewerDocuments(username);
         return ResponseEntity.ok(documents);
     }
 
-    @GetMapping("/list/owner")
+    @GetMapping("/list/owned")
     public ResponseEntity<List<DocumentDTO>> listOwnerDocuments(@RequestAttribute("username") String username) {
         List<DocumentDTO> documents = documentService.listOwnerDocuments(username);
         return ResponseEntity.ok(documents);
